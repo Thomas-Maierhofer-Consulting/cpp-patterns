bec93eab874aa869ab9d63b76199cd3e77e9eed8
Thomas Maierhofer <thomas@maierhofer.de>
2022-07-26 18:49:19 +0200

    Enable clang-tidy, clang-format and compiler explorer integration

--- src/object_layout_deduction/main.cpp
+++ src/object_layout_deduction/main.cpp
@@ -1,58 +1,60 @@
 #include <iostream>
 
-template <typename... T>
-class object_layout_test;  // Not defined to produce an error if instanciated
+template<typename... T>
+class object_layout_test; // Not defined to produce an error if instantiated
 
 // Common part - without (additional) data
-template <>
-class object_layout_test<void>
- {
-    public:
-        object_layout_test(){} 
+template<>
+class object_layout_test<void> {
+public:
+    object_layout_test() = default;
 };
 
+template<typename T>
+class object_layout_test<T> : public object_layout_test<void> {
+public:
+    // Some additional Constructor
+    explicit object_layout_test(const T& value)
+        : additional_(value)
+    {
+    }
 
-template <typename T>
-class object_layout_test<T> : public object_layout_test<void>
-{
-    public:
-        // Some additional Constructor
-        object_layout_test(const T& value) : additional_(value) {}
+    // Some additional methods
+    void set_data(const T& value) { additional_ = value; }
+    T get_data() { return additional_; }
 
-        // Some additional methods
-        void set_data(const T& value) {additional_ = value;}
-        T get_data() {return additional_;}
-
-    private:
-        T additional_;  // Some additional Data
+private:
+    T additional_; // Some additional Data
 };
 
-// Deduction guidelines 
-object_layout_test() -> object_layout_test<void>; // parameterless constructor
-
+// Deduction guidelines
 template<typename T>
 object_layout_test(T) -> object_layout_test<T>; // Generic one argument constructor
 
-object_layout_test(float) -> object_layout_test<double>; // float is deduced as double
+object_layout_test()->object_layout_test<void>; // parameterless constructor
+object_layout_test(float)->object_layout_test<double>; // float is deduced as double
 
+static constexpr auto DOUBLE_VALUE20 = 20.0;
+static constexpr auto FLOAT_VALUE40 = 40.0F;
+static constexpr auto DOUBLE_VALUE42_42 = 42.42;
+static constexpr auto INT_VALUE10 = 10;
 
 int main()
 {
     // template arguments / class versions are deducible
     object_layout_test objVoid;
-    object_layout_test objDouble1{40.0};
-    object_layout_test objDouble2{20.0f};  // Will be double, not float due to deduction guideline
-    object_layout_test objInt(10);
-    
-    objDouble2.set_data(42.42);
-    objDouble1 = objDouble2;  // Assignable, it is the same type
+    object_layout_test objDouble1 {DOUBLE_VALUE20};
+    object_layout_test objDouble2 {FLOAT_VALUE40}; // Will be double, not float due to deduction guideline
+    object_layout_test objInt(INT_VALUE10);
 
+    objDouble2.set_data(DOUBLE_VALUE42_42);
+    objDouble1 = objDouble2; // Assignable, it is the same type
 
-    std::cout << "objDouble2 data is " << objDouble2.get_data() << std::endl; 
-    std::cout << "sizeof objVoid is " << sizeof(objVoid) << std::endl; 
-    std::cout << "sizeof objDouble1 is " << sizeof(objDouble1) << std::endl; 
-    std::cout << "sizeof objDouble2 is " << sizeof(objDouble2) << std::endl; 
-    std::cout << "sizeof objInt is " << sizeof(objInt) << std::endl; 
+    // std::cout << "objDouble2 data is " << objDouble2.get_data() << std::endl;
+    // std::cout << "sizeof objVoid is " << sizeof(objVoid) << std::endl;
+    // std::cout << "sizeof objDouble1 is " << sizeof(objDouble1) << std::endl;
+    // std::cout << "sizeof objDouble2 is " << sizeof(objDouble2) << std::endl;
+    // std::cout << "sizeof objInt is " << sizeof(objInt) << std::endl;
 
     return sizeof(objVoid);
 }
